9df8e8bf12f9749cd80dcc1d20dea8fbf38c0d4d
oscar100dav <oscar.vargas877@ist17dejulio.edu.ec>
2025-10-25 14:05:25 -0500

    2. Clase Socio: herencia de Persona + atributos especificos

--- src/modelo/Socio.java
+++ src/modelo/Socio.java
@@ -3,12 +3,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package modelo;
-
+//
 /**
  *
  * @author Usuario
  */
- 
+ //SUBCLASE SOCIO
 public class Socio extends Persona{
     private int idSocio;
     private int numCuenta;
