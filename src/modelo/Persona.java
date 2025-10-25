c5a0170b149ad978314cad337186c76994f06ae0
oscar100dav <oscar.vargas877@ist17dejulio.edu.ec>
2025-10-25 14:04:59 -0500

    1. Clase Persona: definición de atributos y metodos base

--- src/modelo/Persona.java
+++ src/modelo/Persona.java
@@ -3,11 +3,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package modelo;
-
+//
 /**
  *
  * @author Usuario
  */
+//CLASE PADRE
 public class Persona {
     //atributos
    private int idPersona;
